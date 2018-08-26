399efda96da2f03e76c1ece796464dbb06277c8f
Shadman Kolahzary <info@kolahzary.ir>
2018-08-27 00:21:49 +0430

    Removed Unnecessary Usings

--- CsExcelToXml/CsExcelToXml/Program.cs
+++ CsExcelToXml/CsExcelToXml/Program.cs
@@ -3,10 +3,6 @@ using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace CsExcelToXml
